9b1d019542c36197561120757dab17cfa427fd25
tom <tom@sapletta.com>
2020-06-08 18:49:21 +0200

    better version

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-var_dump($_SERVER['REQUEST_URI']);
-die;
+//var_dump($_SERVER['REQUEST_URI']);
+//die;
 #remove the directory path we don't want
 $request  = str_replace("/envato/pretty/php/", "", $_SERVER['REQUEST_URI']);
 
 #split the path by '/'
-$params     = split("/", $request);
-$safe_pages = array("json", "script", "thread");
+$params     = preg_split("/", $request);
+$safe_pages = ["json", "script"];
 
 if(in_array($params[0], $safe_pages)) {
     include($params[0].".php");
