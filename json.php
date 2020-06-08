966f3e5cd790c00e30c9f34288f581b11f224165
tom <tom@sapletta.com>
2020-06-08 17:04:20 +0200

    better version

--- json.php
+++ json.php
@@ -1,7 +1,9 @@
 <?php
 
+// https://get.wapka.pl/json.php?1
+
 $filename = key($_GET);
-if(filename === integer){
-    echo file_get_contents();
+if($filename === integer){
+    echo file_get_contents("1/" . $filename . ".json");
 }
 //var_dump($_GET);
