5f01e2449a9c05d215352bf70ed5e9ac25d1c749
tom <tom@sapletta.com>
2020-06-08 16:57:48 +0200

    better version

---
+++ docs.bat
@@ -0,0 +1,5 @@
+set URL=https://github.com/wapka-pl/git
+::set BROWSER=chrome
+set BROWSER=firefox
+start %BROWSER% %URL%
+::explorer %URL%
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+$filename = key($_GET);
+if(filename === integer){
+    echo file_get_contents()
+}
+var_dump($_GET);
