4015b243346f7fe22138db809c4eac05e169c41e
tom <tom@sapletta.com>
2020-06-08 21:26:20 +0200

    better version

--- index.php
+++ index.php
@@ -19,17 +19,21 @@ function replace_first($needle, $replace, $haystack)
 #remove the directory path we don't want
 //$request = $_SERVER['REQUEST_URI'];
 //$request = str_replace("/", "", $_SERVER['REQUEST_URI']);
-$filename = replace_first("/", "", $_SERVER['REQUEST_URI']);
+$filename = $_SERVER['REQUEST_URI'];
+$filename = replace_first("index.php", "", $filename);
+$filename = replace_first("/", "", $filename);
 #split the path by '/'
 
 $params = explode('.', $filename);
 $id = $_GET[$params[0]] = $params[0];
-//var_dump($request, $params, $_GET);
+//var_dump($id, $filename, $params, $_GET);
 //die;
 $safe_pages = ["json", "script", "pack.js", "js"];
 //$last_key = count($params) - 1;
 //$extension = $params[$last_key];
 $extension = replace_first($id . ".", "", $filename);
+//var_dump($extension);
+//die;
 if (in_array($extension, $safe_pages)) {
     include($extension . ".php");
 } else {
--- pack.js.php
+++ pack.js.php
@@ -4,19 +4,110 @@
 // https://get.wapka.pl/1.js
 include_once('header/js.php');
 
-$dir    = '.';
+function get_include_contents($filename)
+{
+    if (is_file($filename)) {
+        ob_start();
+        include($filename);
+        $contents = ob_get_contents();
+        ob_end_clean();
+        return $contents;
+    }
+    return false;
+}
+
+function downloadFromJson($jsonfile, $filter = 'js')
+{
+    $json_string = file_get_contents($jsonfile);
+//    var_dump($json_string);
+    $json_array = json_decode($json_string, true);
+    foreach ($json_array as $tag => $list) {
+//        var_dump($tag, $list);
+        foreach ($list as $id => $url) {
+            $info = pathinfo($url);
+//            var_dump($info);
+            if ($info["extension"] === $filter) {
+                $url_base64 = base64_encode($url);
+                $path = 'pack' . DIRECTORY_SEPARATOR . $url_base64 . '.txt';
+
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $data = curl_exec($ch);
+                curl_close($ch);
+
+                file_put_contents($path, $data);
+            }
+        }
+    }
+}
+
+function loadFromJson($jsonfile, $filter = 'js')
+{
+    $json_string = file_get_contents($jsonfile);
+//    var_dump($json_string);
+    $json_array = json_decode($json_string, true);
+    $data = '';
+    foreach ($json_array as $tag => $list) {
+//        var_dump($tag, $list);
+        foreach ($list as $id => $url) {
+            $info = pathinfo($url);
+//            var_dump($info);
+//            $_SERVER['DOCUMENT_ROOT']
+            $path = realpath('.');
+            if ($info["extension"] === $filter) {
+                $url_base64 = base64_encode($url);
+                $path .= DIRECTORY_SEPARATOR . 'pack' . DIRECTORY_SEPARATOR . $url_base64 . '.txt';
+                echo $path;
+//                echo $data = ' // ' . $url;
+                echo $data .= file_get_contents($path);
+//                echo $data .= get_include_contents($path);
+            }
+        }
+    }
+    return $data;
+}
+
+function removeFromJson($jsonfile, $filter = 'js')
+{
+    $json_string = file_get_contents($jsonfile);
+//    var_dump($json_string);
+    $json_array = json_decode($json_string, true);
+    $data = '';
+    foreach ($json_array as $tag => $list) {
+//        var_dump($tag, $list);
+
+        foreach ($list as $id => $url) {
+
+            $info = pathinfo($url);
+//            var_dump($info);
+            if ($info["extension"] === $filter) {
+                unset($json_array[$tag][$id]);
+            }
+        }
+    }
+
+    return json_encode($json_array);
+}
+
+$dir = '.';
 $dirs = scandir($dir);
 //var_dump($_GET);
-$filename = (int) key($_GET);
-if(in_array($filename, $dirs)){
+$filename = (int)key($_GET);
+if (in_array($filename, $dirs)) {
+
     echo "//  https://get.wapka.pl/$filename.js \n";
 
 //    echo file_get_contents( "//load.jloads.com/load.js");
-    echo file_get_contents( "load.js");
+    echo file_get_contents("load.js");
+
+    downloadFromJson($filename . "/" . "jloads.json", 'js');
+    echo loadFromJson($filename . "/" . "jloads.json", 'js');
+
     echo "var json =";
-    echo file_get_contents( $filename . "/" . "jloads.json");
+    echo removeFromJson($filename . "/" . "jloads.json", "js");
+//    echo file_get_contents( $filename . "/" . "jloads.json");
     echo ";";
-    echo file_get_contents( "debug.js");
-    echo file_get_contents( "loadAll.js");
+    echo file_get_contents("debug.js");
+    echo file_get_contents("loadAll.js");
 }
 exit();
