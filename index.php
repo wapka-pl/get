5915ad2d1ae3829b6c56c1f648fe7ed3a5c4f1cb
tom <tom@sapletta.com>
2020-06-08 19:28:11 +0200

    better version

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $request = replace_first("/", "", $_SERVER['REQUEST_URI']);
 $params = explode('.', $request);
 $_GET[$params[0]] = $params[0];
 var_dump($request, $params, $_GET);
-//die;
+die;
 $safe_pages = ["json", "script", "js"];
 
 if (in_array($params[1], $safe_pages)) {
