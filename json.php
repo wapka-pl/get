d0f2dcb5875988f966ff9e641eeedbad398c31b6
tom <tom@sapletta.com>
2020-06-08 17:09:21 +0200

    better version

--- json.php
+++ json.php
@@ -4,9 +4,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // https://get.wapka.pl/json.php?1
+$dir    = '/';
+$dirs = scandir($dir);
 
-$filename = key($_GET);
-if($filename === integer){
+$filename = (int) key($_GET);
+if(in_array($filename, $dirs)){
     echo file_get_contents( $filename . "/" . "jloads.json");
 }
 //var_dump($_GET);
