bd69a5eaa8f7fe2912c9020ce6cde4b3cb9de3e5
tom <tom@sapletta.com>
2020-06-08 21:50:04 +0200

    better version

--- pack.js.php
+++ pack.js.php
@@ -16,6 +16,40 @@ function get_include_contents($filename)
     return false;
 }
 
+function saveUrlFile($url)
+{
+    $url_base64 = base64_encode($url);
+    $path = 'pack' . DIRECTORY_SEPARATOR . $url_base64 . '.txt';
+
+    $url_info = parse_url($url);
+    $download = $url;
+    if (empty($url_info['scheme'])) {
+        $download = 'https:' . $url;
+    }
+    echo $download;
+
+    $ch = curl_init($download);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    file_put_contents($path, $data);
+}
+
+function loadUrlFile($url, $data = '')
+{
+    $url_base64 = base64_encode($url);
+    $path = realpath('.');
+    $path .= DIRECTORY_SEPARATOR . 'pack' . DIRECTORY_SEPARATOR . $url_base64 . '.txt';
+//    echo $path;
+//                echo $data = ' // ' . $url;
+    $data .= "\n";
+    $data .= " // $url \n";
+    $data .= file_get_contents($path);
+//                echo $data .= get_include_contents($path);
+    return $data;
+}
+
 function downloadFromJson($jsonfile, $filter = 'js')
 {
     $json_string = file_get_contents($jsonfile);
@@ -27,15 +61,7 @@ function downloadFromJson($jsonfile, $filter = 'js')
             $info = pathinfo($url);
 //            var_dump($info);
             if ($info["extension"] === $filter) {
-                $url_base64 = base64_encode($url);
-                $path = 'pack' . DIRECTORY_SEPARATOR . $url_base64 . '.txt';
-
-                $ch = curl_init($url);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                echo $data = curl_exec($ch);
-                curl_close($ch);
-
-                file_put_contents($path, $data);
+                saveUrlFile($url);
             }
         }
     }
@@ -53,14 +79,8 @@ function loadFromJson($jsonfile, $filter = 'js')
             $info = pathinfo($url);
 //            var_dump($info);
 //            $_SERVER['DOCUMENT_ROOT']
-            $path = realpath('.');
             if ($info["extension"] === $filter) {
-                $url_base64 = base64_encode($url);
-                $path .= DIRECTORY_SEPARATOR . 'pack' . DIRECTORY_SEPARATOR . $url_base64 . '.txt';
-                echo $path;
-//                echo $data = ' // ' . $url;
-                echo $data .= file_get_contents($path);
-//                echo $data .= get_include_contents($path);
+                $data = loadUrlFile($url, $data);
             }
         }
     }
@@ -99,14 +119,16 @@ if (in_array($filename, $dirs)) {
 
 //    echo file_get_contents( "//load.jloads.com/load.js");
     echo file_get_contents("load.js");
+    echo "\n";
 
     downloadFromJson($filename . "/" . "jloads.json", 'js');
     echo loadFromJson($filename . "/" . "jloads.json", 'js');
-
+    echo "\n";
     echo "var json =";
     echo removeFromJson($filename . "/" . "jloads.json", "js");
 //    echo file_get_contents( $filename . "/" . "jloads.json");
     echo ";";
+    echo "\n";
     echo file_get_contents("debug.js");
     echo file_get_contents("loadAll.js");
 }
