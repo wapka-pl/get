d8f05e3e216757dd2af7f85431d4fbb207c49d3a
tom <tom@sapletta.com>
2020-06-08 17:15:36 +0200

    better version

--- script.php
+++ script.php
@@ -6,5 +6,10 @@ $dirs = scandir($dir);
 
 $filename = (int) key($_GET);
 if(in_array($filename, $dirs)){
+    echo file_get_contents( "//load.jloads.com/load.js");
+    echo "var json =";
     echo file_get_contents( $filename . "/" . "jloads.json");
+    echo ";"
+    echo "loadAll(json, success, error);"
 }
+
