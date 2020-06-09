8c176790380c1752abced0dfeef09b6f29496fb9
tom <tom@sapletta.com>
2020-06-09 14:42:04 +0200

    better version

--- load.js
+++ load.js
@@ -840,7 +840,8 @@ function loadAll(json, success, error, mapFunction) {
     console.log(' loadAll', ' json ', json, Object.keys(json).length, Object.keys(json)[0]);
 
     if (Object.keys(json).length === 1) {
-        getOne(json, Object.keys(json)[0], mapFunction, success, error)
+        var i = Object.keys(json)[0];
+        getOne(json[i], i, mapFunction, success, error)
     } else {
         for (var i in json) {
             var object = json[i];
@@ -851,49 +852,67 @@ function loadAll(json, success, error, mapFunction) {
 
 }
 
+var elem = document.body;
+var mapFunction = {
+    'js': 'js',
+    'css': 'css',
+    'css2': 'css',
+    'css3': 'css',
+    'png': 'img',
+    'bmp': 'img',
+    'jpg': 'img',
+    'gif': 'img',
+    'htm': 'html',
+    'html': 'html',
+    'html5': 'html'
+}
+
+
 function getOne(object, i, mapFunction, success, error) {
     console.log('loadAll getOne ', ' object i ', object, i);
 
-    const elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i);
-    console.log('loadAll getOne ', ' elem ', elem);
+    elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i) || document.body;
+    console.log('loadAll getOne ', ' elem ', elem, !isEmpty(elem));
 
     if (!isEmpty(elem)) {
+        console.log('loadAll getOne ', ' !isEmpty ', elem, !isEmpty(elem));
         loadContentByUrls(object, elem, mapFunction, success, error);
+        return success(elem);
     } else {
-        document.onload = function () {
+        document.addEventListener("DOMContentLoaded", function () {
+            ReadyHtml(object, i, elem, mapFunction, success, error);
+        });
 
-            const elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i);
-
-            log('loadAll getOne document.onload ', ' wait for DOM tree if not exist jet ', i, e);
-
-            if (!isEmpty(elem)) {
-                loadContentByUrls(object, elem, mapFunction, success, error);
-            } else {
-                error(elem);
-            }
-        }
     }
+    return error(elem);
 }
 
 function loadContentByUrls(object, elem, mapFunction, success, error) {
+
     var jloads = new Load(elem, success, error);
 
     this.constructor.name = 'loadAll loadContent';
 
+    console.log(this.constructor.name, ' isArray object, elem, mapFunction', object, isArray(object), elem, mapFunction);
+
+
     if (isArray(object)) {
         var url = '';
         for (var id in object) {
+            console.log('loadContentByUrls isArray', ' object ', object);
             url = object[id];
+            console.log('loadContentByUrls isArray', ' url ', url, typeof url === 'string');
+
             if (typeof url === 'string') {
                 try {
                     var funcName = getFunctionName(url, mapFunction);
-                    log(this.constructor.name, ' funcName ', funcName);
+                    console.log(this.constructor.name, ' funcName ', funcName);
                     // console.log(funcName, url, elem);
                     jloads[funcName](url);
                     success(url);
                 } catch (e) {
-                    log(this.constructor.name, ' elem ', elem);
-                    log(this.constructor.name, ' ERROR ', e);
+                    console.log(this.constructor.name, ' ERROR elem ', elem);
+                    console.log(this.constructor.name, ' ERROR e ', e);
                     error(e);
                 }
 
@@ -901,5 +920,24 @@ function loadContentByUrls(object, elem, mapFunction, success, error) {
                 // elem.appendChild(url, funcName);
             }
         }
+    } else {
+        console.log(this.constructor.name, ' isArray ERROR object', object);
+        error(object);
+    }
+}
+
+
+function ReadyHtml(object, i, elem, mapFunction, success, error) {
+
+    elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i) || document.body;
+
+    console.log('ReadyHtml getOne ', ' elem ', elem, !isEmpty(elem));
+    console.log('ReadyHtml getOne ', ' i ', i);
+
+    if (!isEmpty(elem)) {
+        loadContentByUrls(object, elem, mapFunction, success, error);
+        return success(elem);
+    } else {
+        return error(elem);
     }
 }
