ec59de843abfde03e6b8bf8e9ae2744145424c31
tom <tom@sapletta.com>
2020-06-11 20:30:38 +0200

    info about version

--- 404.php
+++ 404.php
@@ -5,8 +5,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+echo "<br>";
 echo('REQUEST_URI <hr/>');
 var_dump($_SERVER['REQUEST_URI']);
+echo "<br>";
 echo('$_GET <hr/>');
 var_dump($_GET);
 //die;
