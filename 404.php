93d7b283cfd476b6abeff017ddc8f9b31f82052f
tom <tom@sapletta.com>
2020-06-11 20:29:47 +0200

    info about version

--- 404.php
+++ 404.php
@@ -5,9 +5,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo('REQUEST_URI');
+echo('REQUEST_URI <hr/>');
 var_dump($_SERVER['REQUEST_URI']);
-echo('$_GET');
+echo('$_GET <hr/>');
 var_dump($_GET);
 //die;
 $safe_pages = ["json", "b64", "script", "js"];
