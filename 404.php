4c69653f06e4b4dd2944ea1d3c52a395c0a865e4
tom <tom@sapletta.com>
2020-06-11 20:31:28 +0200

    info about version

--- 404.php
+++ 404.php
@@ -1,15 +1,14 @@
 PAGE IS NOT EXISTING
-<hr/>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "<br>";
-echo('REQUEST_URI <hr/>');
+echo "<hr/><br>";
+echo('REQUEST_URI');
 var_dump($_SERVER['REQUEST_URI']);
-echo "<br>";
-echo('$_GET <hr/>');
+echo "<hr/><br>";
+echo('GET');
 var_dump($_GET);
 //die;
 $safe_pages = ["json", "b64", "script", "js"];
