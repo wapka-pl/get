abb47376da4792bb05be19a1a87bf1bab58ffd8f
tom <tom@sapletta.com>
2020-06-13 14:46:08 +0200

    rename to jloads-url.js

--- push.bat
+++ push.bat
@@ -4,5 +4,5 @@ SetLocal EnableDelayedExpansion
 for /f %%i in ('dir /b /od Ticket\*.md') do set filename=%%i
 for /f "delims==" %%a in (Ticket\%filename%) do set message=%%a
 @ECHO ON
-message.bat && git add . && git commit -m "%message%" && git push origin master
+message.bat && git add . && git commit -m "%message%" && git push origin master && timeout /T 1 && remote_update.bat
 ::&& docs.bat
---
+++ remote_update.bat
@@ -0,0 +1,2 @@
+cd F:\PycharmProjects\faas-ovh-api\
+py deploy.py get.wapka.pl update.sh
